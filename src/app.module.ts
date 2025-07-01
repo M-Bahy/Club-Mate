129ebcef0701e000c4033177dd4d58d00ad077f2
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-01 18:18:44 +0300

    create sport resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './supabase/supabase.module';
 import { MemberModule } from './member/member.module';
+import { SportModule } from './sport/sport.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MemberModule } from './member/member.module';
     }),
     SupabaseModule,
     MemberModule,
+    SportModule,
   ],
   controllers: [AppController],
   providers: [AppService],
