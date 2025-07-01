a71328a04be63491ae4a5ebc8950eb8e965952ac
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-01 17:11:23 +0300

    Implement Member entity with TypeORM decorators and properties

--- src/member/entities/member.entity.ts
+++ src/member/entities/member.entity.ts
@@ -1,1 +1,44 @@
-export class Member {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Gender } from '../enums/gender.enum';
+
+@Entity()
+export class Member {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('members')
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({
+    type: 'enum',
+    enum: Gender,
+  })
+  gender: Gender;
+
+  @Column({
+    type: 'date',
+    nullable: true,
+  })
+  dob?: Date;
+
+  @Column({
+    type: 'uuid',
+    nullable: true,
+  })
+  associatedMemberId?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
