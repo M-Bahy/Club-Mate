b6979e44cece76658e948dc7b8b1b5e9538d2a01
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-01 19:54:15 +0300

    Implemented CreateSportDto

--- src/sport/dto/create-sport.dto.ts
+++ src/sport/dto/create-sport.dto.ts
@@ -1,1 +1,22 @@
-export class CreateSportDto {}
+import {
+  IsString,
+  IsEnum,
+  IsDateString,
+  IsOptional,
+  IsUUID,
+  IsNumber,
+  IsPositive,
+} from 'class-validator';
+import { Gender } from '../enums/gender.enum';
+
+export class CreateSportDto {
+    @IsString()
+    name: string;
+    
+    @IsNumber()
+    @IsPositive()
+    price: number;
+    
+    @IsEnum(Gender)
+    allowedGender: Gender;
+}
