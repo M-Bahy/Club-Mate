80c37a0cb3816b75834f5452ecd9ac13b27ede87
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-01 18:19:47 +0300

    Add SupabaseModule import to SportModule

--- src/sport/sport.module.ts
+++ src/sport/sport.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SportService } from './sport.service';
 import { SportController } from './sport.controller';
+import { SupabaseModule } from '../supabase/supabase.module';
+
 
 @Module({
+  imports: [SupabaseModule],
   controllers: [SportController],
   providers: [SportService],
 })
