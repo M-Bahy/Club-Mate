d50ef974a37994509e1280626e1a11572282cfcf
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-02 03:57:46 +0300

    service imports

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
 import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
+import { SupabaseService } from '../supabase/supabase.service';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { MemberService } from '../member/member.service';
+import { SportService } from '../sport/sport.service';
+import { SubscriptionType } from './enums/subscription-type.enum';
+import { Subscription } from './entities/subscription.entity';
 
 @Injectable()
 export class SubscriptionService {
-  create(createSubscriptionDto: CreateSubscriptionDto) {
+
+  private supabase: SupabaseClient;
+
+  constructor(
+    private readonly supabaseService: SupabaseService,
+    private readonly memberService: MemberService,
+    private readonly sportService: SportService,
+  ) {}
+
+  onModuleInit() {
+    this.supabase = this.supabaseService.getClient();
+    if (!this.supabase) {
+      throw new Error('Failed to initialize Supabase client');
+    }
+  }
+
+  async create(createSubscriptionDto: CreateSubscriptionDto) {
     return 'This action adds a new subscription';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all subscription`;
   }
 
-  findOne(id: number) {
+  async findOne(id: string) {
     return `This action returns a #${id} subscription`;
   }
 
-  update(id: number, updateSubscriptionDto: UpdateSubscriptionDto) {
+  async update(id: string, updateSubscriptionDto: UpdateSubscriptionDto) {
     return `This action updates a #${id} subscription`;
   }
 
-  remove(id: number) {
+  async remove(id: string) {
     return `This action removes a #${id} subscription`;
   }
 }
