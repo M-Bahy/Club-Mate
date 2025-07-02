63ae5ebffc4d80f3d0a94cbac65341dbde4c4077
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-02 03:32:16 +0300

    Implement Subscription entity

--- src/subscription/entities/subscription.entity.ts
+++ src/subscription/entities/subscription.entity.ts
@@ -1,1 +1,44 @@
-export class Subscription {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  Unique,
+} from 'typeorm';
+import { Member } from '../../member/entities/member.entity';
+import { Sport } from '../../sport/entities/sport.entity';
+import { SubscriptionType } from '../enums/subscription-type.enum'; 
+
+@Entity('subscriptions')
+@Unique(['memberId', 'sportId']) // Ensures one subscription per member per sport
+export class Subscription {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('uuid')
+  memberId: string;
+
+  @Column('uuid')
+  sportId: string;
+
+  @Column('date')
+  subscriptionDate: Date;
+
+    @Column({
+        type: 'enum',
+        enum: SubscriptionType,
+    })
+    subscriptionType: SubscriptionType;
+
+  // Relations (useful for queries)
+  @ManyToOne(() => Member)
+  @JoinColumn({ name: 'memberId' })
+  member: Member;
+
+  @ManyToOne(() => Sport)
+  @JoinColumn({ name: 'sportId' })
+  sport: Sport;
+}
---
+++ src/subscription/enums/subscription-type.enum.ts
@@ -0,0 +1,4 @@
+export enum SubscriptionType {
+  GROUP = 'group',
+  PRIVATE = 'private',
+}
