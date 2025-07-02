f7d6419f4e95c5672a12920c2c763cdbbe66bd60
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-03 00:31:27 +0300

    Refactor findAll method to omit wildcard

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -56,7 +56,7 @@ export class SubscriptionService {
   async findAll() : Promise<Subscription[]> {
     const { data, error } = await this.supabase
       .from('subscriptions')
-      .select('*');
+      .select();
 
     if (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
