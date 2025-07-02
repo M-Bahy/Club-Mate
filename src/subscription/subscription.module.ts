24e356f9757eaf31b9fbe90182c0ed5c69324248
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-02 03:18:14 +0300

    Add module imports to SubscriptionModule

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
+import { SupabaseModule } from '../supabase/supabase.module';
+import { MemberModule } from '../member/member.module';
+import { SportModule } from '../sport/sport.module';
 
 @Module({
+  imports: [SupabaseModule, MemberModule, SportModule],
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
 })
